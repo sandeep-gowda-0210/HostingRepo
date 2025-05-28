d6287122d03367d60b750aaabf422a5d687cea45
sandeep-gowda-0210 <sandeepgowda0210@gmail.com>
2025-05-28 19:18:22 +0530

    Working progress 2 - message scheduler

--- air/src/services/chatService.ts
+++ air/src/services/chatService.ts
@@ -192,11 +192,14 @@ export const setScheduleMessage = async (token: string, scheduleData: scheduleMe
   return error;
 }
 
-export function getLocalDateTimeString() {
+ function getLocalDateTimeString() {
   const now = new Date();
-  const offset = now.getTimezoneOffset();
-  const localTime = new Date(now.getTime() - offset * 60 * 1000);
-  return localTime.toISOString().slice(0, 16);
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  return `${year}-${month}-${day}T${hours}:${minutes}`;
 }
 
 export const sendScheduledMessage = async () => {
